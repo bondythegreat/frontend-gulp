d7038dfbf48a6bbd417679db30168f4777b6b06e
bondan mintardjo <bondan.mintardjo@42interactive.com>
2022-06-25 14:37:30 +0800

    gulp browsersync. lightbox2. html and assets of answer your body needs

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require("gulp"),
   sass = require("gulp-sass")(require("sass")),
+  browserSync = require("browser-sync").create(),
   concat = require("gulp-concat"),
   uglify = require("gulp-uglify"),
   minify = require("gulp-minify-css"),
@@ -11,6 +12,7 @@ gulp.task("pack-vendors-js", function () {
   return gulp
     .src([
       "node_modules/jquery/dist/jquery.min.js",
+      "node_modules/lightbox2/dist/js/lightbox-plus-jquery.min.js",
       "node_modules/bootstrap/dist/js/bootstrap.bundle.js",
     ])
     .pipe(mode.development(sourcemaps.init()))
@@ -27,12 +29,16 @@ gulp.task("pack-js", function () {
     .pipe(concat("bundle.min.js"))
     .pipe(mode.production(uglify()))
     .pipe(mode.development(sourcemaps.write()))
-    .pipe(gulp.dest("build/js"));
+    .pipe(gulp.dest("build/js"))
+    .pipe(browserSync.stream());
 });
 
 gulp.task("pack-vendors-css", function () {
   return gulp
-    .src(["node_modules/bootstrap/dist/css/bootstrap.css"])
+    .src([
+      "node_modules/bootstrap/dist/css/bootstrap.css",
+      "node_modules/lightbox2/dist/css/lightbox.min.css",
+    ])
     .pipe(sass().on("error", sass.logError))
     .pipe(concat("vendors.min.css"))
     .pipe(mode.production(minify()))
@@ -45,17 +51,23 @@ gulp.task("pack-css", function () {
     .pipe(sass().on("error", sass.logError))
     .pipe(concat("bundle.min.css"))
     .pipe(mode.production(minify({ processImport: false })))
-    .pipe(gulp.dest("build/css"));
+    .pipe(gulp.dest("build/css"))
+    .pipe(browserSync.stream());
 });
 
 gulp.task("pack-images", async function () {
   gulp.src("src/img/**/*").pipe(imagemin()).pipe(gulp.dest("build/img"));
 });
 
-gulp.task("watch", function () {
+gulp.task("serve", function () {
+  browserSync.init({
+    server: "./",
+  });
+
   gulp.watch("src/scss/**/*", gulp.series("pack-css"));
   gulp.watch("src/img/**/*", gulp.series("pack-images"));
   gulp.watch("src/js/*.js", gulp.series("pack-js"));
+  gulp.watch("*.html").on("change", browserSync.reload);
 });
 
 gulp.task(
