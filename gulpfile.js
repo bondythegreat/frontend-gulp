a6a1fcca29c55e0c81a03acd815ab1244b208f22
bondan mintardjo <bondan.mintardjo@42interactive.com>
2022-06-25 11:37:46 +0800

    gulp-sass install. basic structure of src/js and src/scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require("gulp"),
+  sass = require("gulp-sass")(require("sass")),
   concat = require("gulp-concat"),
   uglify = require("gulp-uglify"),
   minify = require("gulp-minify-css"),
+  imagemin = require("gulp-imagemin"),
   sourcemaps = require("gulp-sourcemaps"),
   mode = require("gulp-mode")();
 
@@ -28,25 +30,41 @@ gulp.task("pack-js", function () {
     .pipe(gulp.dest("build/js"));
 });
 
-gulp.task("pack-css", function () {
+gulp.task("pack-vendors-css", function () {
   return gulp
-    .src(["src/scss/style.scss"])
-    .pipe(concat("bundle.min.css"))
+    .src(["node_modules/bootstrap/dist/css/bootstrap.css"])
+    .pipe(sass().on("error", sass.logError))
+    .pipe(concat("vendors.min.css"))
     .pipe(mode.production(minify()))
     .pipe(gulp.dest("build/css"));
 });
 
-gulp.task("pack-images", function () {
-  gulp.src("src/img/**/*").pipe(imagemin()).pipe(gulp.dest("dist/img"));
+gulp.task("pack-css", function () {
+  return gulp
+    .src(["src/scss/style.scss"])
+    .pipe(sass().on("error", sass.logError))
+    .pipe(concat("bundle.min.css"))
+    .pipe(mode.production(minify({ processImport: false })))
+    .pipe(gulp.dest("build/css"));
+});
+
+gulp.task("pack-images", async function () {
+  gulp.src("src/img/**/*").pipe(imagemin()).pipe(gulp.dest("build/img"));
 });
 
 gulp.task("watch", function () {
-  gulp.watch("src/css/*.css", gulp.series("pack-css"));
+  gulp.watch("src/scss/**/*", gulp.series("pack-css"));
   gulp.watch("src/img/**/*", gulp.series("pack-images"));
   gulp.watch("src/js/*.js", gulp.series("pack-js"));
 });
 
 gulp.task(
   "default",
-  gulp.series("pack-vendors-js", "pack-js", "pack-css", "pack-images")
+  gulp.series(
+    "pack-vendors-js",
+    "pack-js",
+    "pack-vendors-css",
+    "pack-css",
+    "pack-images"
+  )
 );
